867be15f7ab1564b862df1bff1a115e7bda5fdfb
InukChoi <iu.choi3197@gmail.com>
2025-02-26 14:09:37 +0900

    [Feat] 게시글 작성 api 연동 #3

--- src/components/BoardRegister.vue
+++ src/components/BoardRegister.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { useBoardStore } from '../stores/useBoardStore';
 
-const newTitle = ref('');
-const newContent = ref('');
-const newAuthor = ref('');
+const title = ref('');
+const content = ref('');
+const writer = ref('');
 const router = useRouter();
+const boardStore = useBoardStore();
 
-const submitPost = () => {
-    if (newTitle.value.trim() !== '' && newContent.value.trim() !== '' && newAuthor.value.trim() !== '') {
-        
+const submitPost = async () => {
+    if (title.value.trim() !== '' && content.value.trim() !== '' && writer.value.trim() !== '') {
+        const response = await boardStore.registerPost({title: title.value, content: content.value, writer: writer.value});
         alert('게시글이 등록되었습니다.');
         router.push('/');
     }
@@ -21,11 +23,11 @@ const submitPost = () => {
         <h1 class="text-2xl font-bold mb-4">게시글 작성</h1>
 
         <div class="mb-4 flex space-x-2">
-            <input v-model="newTitle" type="text" placeholder="제목을 입력하세요" class="border p-2 flex-grow rounded-md" />
-            <input v-model="newAuthor" type="text" placeholder="작성자를 입력하세요" class="border p-2 w-1/4 rounded-md" />
+            <input v-model="title" type="text" placeholder="제목을 입력하세요" class="border p-2 flex-grow rounded-md" />
+            <input v-model="writer" type="text" placeholder="작성자를 입력하세요" class="border p-2 w-1/4 rounded-md" />
         </div>
 
-        <textarea v-model="newContent" placeholder="내용을 입력하세요" class="border p-2 w-full rounded-md mb-2"></textarea>
+        <textarea v-model="content" placeholder="내용을 입력하세요" class="border p-2 w-full rounded-md mb-2"></textarea>
 
         <div class="flex justify-end">
             <button @click="submitPost" class="mt-2 bg-blue-500 text-white p-2 rounded-md">등록</button>
